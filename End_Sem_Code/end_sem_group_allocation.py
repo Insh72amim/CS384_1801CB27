b350cc64acbeed16ea903f1eefa8eee58f5917b0
Insh72amim <amim.insherah@gmail.com>
2020-11-30 16:48:40 +0530

    data strength

--- End_Sem_Code/end_sem_group_allocation.py
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -1,8 +1,32 @@
-
+import pandas as pd
+import re
+pattern=re.compile(r'\d+(\D+)\d+')
 def group_allocation(filename, number_of_groups):
-    # Entire Logic 
-	# You can add more functions, but in the test case, we will only call the group_allocation() method,
+    datafile = pd.read_csv(filename)
+    unique_branches = set()
+
+    for x in datafile['Roll']:
+        if pattern.search(x):
+            unique_branches.add(pattern.search(x).group(1))
+
+    print(unique_branches)
+    files = dict()
+    for x in unique_branches:
+        files[x] = dict()
+        files[x]['Roll'] = list()
+        files[x]['Name'] = list()
+        files[x]['Email'] = list()
+
+    for post in range(len(datafile['Roll'])):
+        branch = pattern.search(datafile['Roll'][post]).group(1)
+        files[branch]['Roll'].append(datafile['Roll'][post])
+        files[branch]['Name'].append(datafile['Name'][post])
+        files[branch]['Email'].append(datafile['Email'][post])
+
+    datastrength = {}
+    datastrength['STRENGTH'] = []
+    datastrength['BRANCH_CODE'] = []
 
 filename = "Btech_2020_master_data.csv"
-number_of_groups = 12 
+number_of_groups = 12
 group_allocation(filename, number_of_groups)
