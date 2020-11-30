3a7e54da98655911d179f263fab39461d21c02f5
Insh72amim <amim.insherah@gmail.com>
2020-11-30 16:52:34 +0530

    New data

--- End_Sem_Code/end_sem_group_allocation.py
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -27,6 +27,23 @@ def group_allocation(filename, number_of_groups):
     datastrength['STRENGTH'] = []
     datastrength['BRANCH_CODE'] = []
 
+    for x in unique_branches:
+        new_files = {}
+        new_files['Roll'] = list()
+        new_files['Roll'] = files[x]['Roll'].copy()
+        new_files['Name'] = list()
+        new_files['Name'] = files[x]['Name'].copy()
+        new_files['Email'] = list()
+        new_files['Email'] = files[x]['Email'].copy()
+        data = pd.DataFrame(new_files)
+        data=data.sort_values(['Roll'])
+        data.to_csv(x + '.csv')
+        datastrength['BRANCH_CODE'].append(x)
+        datastrength['STRENGTH'].append(len(files[x]['Roll']))
+
+    newdata = pd.DataFrame(datastrength)
+    newdata = newdata.sort_values(['STRENGTH', 'BRANCH_CODE'])
+
 filename = "Btech_2020_master_data.csv"
 number_of_groups = 12
 group_allocation(filename, number_of_groups)
