236a1fb6de2da8404ebaa2e536635c5b15826d88
Insh72amim <amim.insherah@gmail.com>
2020-11-30 17:43:06 +0530

    Renaming Game of thrones

--- Assignment 5 Web Series Renamer/tutorial05.py
+++ Assignment 5 Web Series Renamer/tutorial05.py
@@ -42,7 +42,24 @@ def rename_FIR(folder_name):
 
 
 def rename_Game_of_Thrones(folder_name):
-    # rename Logic
+    path = os.path.join(os.getcwd(),os.path.join('Subtitles',folder_name))
+    for file in os.listdir(path):
+        try:
+            name = file.split('-')
+            name = [i.strip() for i in name]
+            season = (season_pd - len(rm_zero(name[1].split('x')[0])))*'0' + rm_zero(name[1].split('x')[0])
+            episode = (episode_pd - len(rm_zero(name[1].split('x')[1])))*'0' +rm_zero(name[1].split('x')[1])
+            episode_name = name[-1].split('.')[0].strip()
+            ext = name[-1].split('.')[-1]
+            file_name = name[0] + " - Season " + season + " Episode " + episode + " - " + episode_name + "." + ext
+            file_name_old = os.path.join(path,file)
+            file_name = os.path.join(path,file_name)
+            try:
+                os.rename(file_name_old,file_name)
+            except:
+                os.remove(file_name_old)
+        except :
+            pass
 
 
 def rename_Sherlock(folder_name):
