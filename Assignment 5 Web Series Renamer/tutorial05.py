a14165fab8bdff0d8376b59e027a7203a580757b
Insh72amim <amim.insherah@gmail.com>
2020-11-30 17:44:10 +0530

    Renaming Sherlock

--- Assignment 5 Web Series Renamer/tutorial05.py
+++ Assignment 5 Web Series Renamer/tutorial05.py
@@ -63,7 +63,28 @@ def rename_Game_of_Thrones(folder_name):
 
 
 def rename_Sherlock(folder_name):
-    # rename Logic
+    path = os.path.join(os.getcwd(),os.path.join('Subtitles',folder_name))
+    for file in os.listdir(path):
+        try:
+            name = file.split('.')
+            name = [i.strip() for i in name]
+            if 'E' not in name[1]:
+                season = (season_pd - len(rm_zero(name[1][1:])))*'0' + rm_zero(name[1][1:])
+                episode = (episode_pd - len(rm_zero(name[2][1:])))*'0' + rm_zero(name[2][1:])
+            else:
+                block = name[1].split("E")
+                season = (season_pd - len(rm_zero(block[0][1:])))*'0' + rm_zero(block[0][1:])
+                episode = (episode_pd - len(rm_zero(block[1])))*'0' + rm_zero(block[1])
+            file_name = name[0] + " - Season " + season + " Episode " + episode + "." + name[-1]
+            file_name = os.path.join(path,file_name)
+            file_name_old = os.path.join(path,file)
+            print(file_name)
+            try:
+                os.rename(file_name_old,file_name)
+            except:
+                os.remove(file_name_old)
+        except :
+            pass
 
 
 def rename_Suits(folder_name):
