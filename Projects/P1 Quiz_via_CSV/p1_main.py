842261307b215c18fa76222edf5bee735aa03b1c
Insh72amim <amim.insherah@gmail.com>
2020-12-06 00:43:47 +0530

    Class function is added

--- Projects/P1 Quiz_via_CSV/p1_main.py
+++ Projects/P1 Quiz_via_CSV/p1_main.py
@@ -108,3 +108,80 @@ class screen:
         else:
             messagebox.showerror(
                 "Error", "{} already exists".format(self.reg_rollnumber.get()))
+
+
+class home_screen:
+    def __init__(self, roll, name):
+        self.roll = roll
+        self.name = name
+        mainmaster = Tk()
+        Label(mainmaster, text="Select Quiz", font=(
+            "Helvetica", 20, "bold italic")).pack()
+        selected_quiz = StringVar()
+        for filename in os.listdir("./quiz_wise_questions"):
+            if filename.endswith(".csv"):
+                Radiobutton(mainmaster, text=filename[:-4], variable=selected_quiz,
+                            value=filename, anchor="e", justify=LEFT).pack()
+        Button(mainmaster, text="Click!!", command=lambda: [
+               mainmaster.destroy(), self.start_quiz(selected_quiz.get())]).pack()
+        mainmaster.mainloop()
+
+    def start_quiz(self, filename):
+        quiz_screen = Tk()
+
+        def unat_event(event):
+            self.__get_unattem()
+        quiz_screen.bind("<Control_L><Alt_L><U>", unat_event)
+
+        def goto_event(event):
+            self.__go_to()
+        quiz_screen.bind("<Control_L><Alt_L><G>", goto_event)
+
+        def submit_event(event):
+            self.submit()
+        quiz_screen.bind("<Control_L><Alt_L><F>", submit_event)
+
+        def export_event(event):
+            self.__export_csv()
+        quiz_screen.bind("<Control_L><Alt_L><E>", export_event)
+        self.filename = filename
+        self.user_mark_ds = datastore.users_marks_datastore()
+        self.q_ds = datastore.quiz_datastore(filename).get_quiz()
+        info_frame = Frame(quiz_screen)
+        info_frame.pack(side=TOP)
+        self.channel = queue.Queue()
+        self.max_timer = self.q_ds.get("q_time")
+        thread = threading.Thread(target=self.timer)
+        Label(info_frame, text="Timer: ").grid(row=0, sticky=W)
+        self.timer_lb = Label(info_frame, text="")
+        self.timer_lb.grid(row=0, column=1)
+        Label(info_frame, text="Roll: ").grid(row=1, sticky=W)
+        Label(info_frame, text=self.roll).grid(row=1, column=1)
+        Label(info_frame, text="Name: ").grid(row=2, sticky=W)
+        Label(info_frame, text=self.name).grid(row=2, column=1)
+        Button(info_frame, text="Unattempted Questions",
+               command=self.__get_unattem).grid(row=3, sticky=W)
+        Button(info_frame, text="Goto Question:",
+               command=self.__go_to).grid(row=3, column=1)
+        Button(info_frame, text="Export Database into CSV",
+               command=self.__export_csv).grid(row=4, sticky=W)
+        self.info_frame = info_frame
+        thread.start()
+        # Question frame
+        self.response = {key: value for key, value in zip(self.q_ds.get(
+            "questions").keys(), [-1]*len(self.q_ds.get("questions")))}
+        self.ques_frame = Frame(quiz_screen)
+        self.ques_frame.pack()
+        self.q_num = 1
+        self.ques = self.create_q(self.q_num)
+        self.opts = self.create_options()
+        self.display_q(self.q_num)
+        self.next_btn = Button(
+            self.ques_frame, text="Save & Next", command=self.next)
+        self.submit_btn = Button(
+            self.ques_frame, text="Final Submit", command=self.submit)
+        self.next_btn.pack(pady=10, padx=5, side=LEFT)
+        self.submit_btn.pack(pady=10, padx=5)
+        #########################################################
+        self.quiz_screen = quiz_screen
+        quiz_screen.mainloop()
